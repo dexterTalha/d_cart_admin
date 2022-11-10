94305a5be2c070962c4aa3f0ef1464075b5c49f7
dextertalha <talhaviet22@gmail.com>
2022-11-10 21:46:10 +0530

    login background responsive web login completed

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -17,97 +17,127 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passController = TextEditingController();
 
+  late Size size;
+
   @override
   Widget build(BuildContext context) {
+    size = MediaQuery.of(context).size;
     return Scaffold(
       body: ResponsiveBuilder(
-        mobile: loginForm(),
-        tablet: Container(
-          color: Colors.green,
-        ),
-        web: Container(
-          color: Colors.blue,
-        ),
+        mobile: loginForm,
+        tablet: desktopLogin,
+        web: desktopLogin,
       ),
     );
   }
 
-  Widget loginForm() {
-    return Container(
-      width: double.maxFinite,
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 20),
-          const Text(
-            "Signin",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 29,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
-          const Text(
-            "Welcome back",
-            style: TextStyle(
-              color: Colors.grey,
-              fontSize: 14,
-            ),
-          ),
-          const SizedBox(height: 30),
-          TextFormFieldComponent(
-            title: "Your Email",
-            hint: "email@address.com",
-            controller: _emailController,
-          ),
-          const SizedBox(height: 20),
-          TextFormFieldComponent(
-            isObscure: isObscure,
-            title: "Password",
-            controller: _passController,
-            hint: "6+ characters required",
-            suffixWidget: GestureDetector(
-              onTap: () {
-                setState(() {
-                  isObscure = !isObscure;
-                });
-              },
-              child: Icon(
-                !isObscure ? Icons.visibility_off : Icons.visibility,
-                color: MyTheme.textFormBorder,
+  Widget get desktopLogin => Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Image.asset(
+                      "assets/png/login_background.png",
+                      height: size.height,
+                      width: size.width * 0.3,
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      "LOGIN SCREEN",
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-          const SizedBox(height: 10),
-          CheckboxListTile(
-            contentPadding: EdgeInsets.zero,
-            checkboxShape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(3),
+            Expanded(
+              flex: 1,
+              child: loginForm,
             ),
-            value: isChecked,
-            title: Text("Remember me"),
-            controlAffinity: ListTileControlAffinity.leading,
-            visualDensity: VisualDensity(horizontal: -4, vertical: -4),
-            onChanged: (xyz) {
-              setState(() {
-                isChecked = xyz ?? false;
-              });
-            },
-          ),
-          const SizedBox(height: 10),
-          CommonButton(
-            text: "Login",
-            textStyle: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w400,
+          ],
+        ),
+      );
+
+  Widget get loginForm => Container(
+        width: double.maxFinite,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20),
+            const Text(
+              "Signin",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 29,
+                fontWeight: FontWeight.w500,
+              ),
             ),
-            onPressed: () {},
-            borderRadius: 8,
-            verticalPadding: 10,
-          ),
-        ],
-      ),
-    );
-  }
+            const Text(
+              "Welcome back",
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 14,
+              ),
+            ),
+            const SizedBox(height: 30),
+            TextFormFieldComponent(
+              title: "Your Email",
+              hint: "email@address.com",
+              controller: _emailController,
+            ),
+            const SizedBox(height: 20),
+            TextFormFieldComponent(
+              isObscure: isObscure,
+              title: "Password",
+              controller: _passController,
+              hint: "6+ characters required",
+              suffixWidget: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isObscure = !isObscure;
+                  });
+                },
+                child: Icon(
+                  !isObscure ? Icons.visibility_off : Icons.visibility,
+                  color: MyTheme.textFormBorder,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            CheckboxListTile(
+              contentPadding: EdgeInsets.zero,
+              checkboxShape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(3),
+              ),
+              value: isChecked,
+              title: Text("Remember me"),
+              controlAffinity: ListTileControlAffinity.leading,
+              visualDensity: VisualDensity(horizontal: -4, vertical: -4),
+              onChanged: (xyz) {
+                setState(() {
+                  isChecked = xyz ?? false;
+                });
+              },
+            ),
+            const SizedBox(height: 10),
+            CommonButton(
+              text: "Login",
+              textStyle: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              ),
+              onPressed: () {},
+              borderRadius: 8,
+              verticalPadding: 10,
+            ),
+          ],
+        ),
+      );
 }
